7b26b60fea386d257f0d00641caecb5a42cb1d9b
Michael Ray <raymichael23@gmail.com>
2016-05-05 20:16:15 -0700

    multiple loop types completed

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-var scores = [
-  {RAY: 10890},
-  {IGN: 9200},
-  {GGA: 10220},
-  {PAB: 12200},
-  {TAB: 11020}
-]
+// var scores = [
+//   1204300,
+//   1203040,
+//   1010230,
+//   1000410,
+//   903193
+// ]
+
 var scoreList = document.querySelector('#highscores');
+var scoreNumberList = document.querySelector('#scoreNumbers');
+// *******for loop works********
+// for (var i=0; i<scores.length; i++){
+//   console.log(scores[i]);
+//   scoreList.innerHTML += '<li>' + scores[i] + '</li>';
+// *******while loop works*******
+// var i = 0;
+// while (i<scores.length) {
+//   console.log(scores[i]);
+//   scoreList.innerHTML += '<li>' + scores[i] + '</li>';
+//   i++;
+// }
+//
+// forEach loop works
+// scores.forEach(function(el){
+//   scoreList.innerHTML += '<li>' + el + '</li>';
+// }
+// )
+
+var playerScores = {
+  RAY: 1009200,
+  MOG: 1030010,
+  LAW: 1040010,
+  ASS: 1200990,
+  POG: 996030
+}
+
+for (key in playerScores) {
+  scoreList.innerHTML += '<li class="names">' + key + '</li>';
+  scoreNumberList.innerHTML += '<li class="scores">' + playerScores[key] + '</li>';
+
+}
